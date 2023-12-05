c4bdf32bf621c1f22f66c3f960adb36d8c52f0da
stani07p <nigel.stanger@otago.ac.nz>
2023-12-05 13:27:42 +1300

    Fixed home-manager version mismatch

--- flake.nix
+++ flake.nix
@@ -6,11 +6,11 @@
         nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
         nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
-        home-manager.url = "github:nix-community/home-manager/master";
-        home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+        home-manager.url = "github:nix-community/home-manager/release-23.11";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs-stable";
 
         darwin.url = "github:lnl7/nix-darwin";
-        darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
+        darwin.inputs.nixpkgs.follows = "nixpkgs-stable";
 
         nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
 
@@ -89,82 +89,6 @@
             darwinConfigurations = processConfigurations {
                 Nigels-Virtual-Machine = darwinSystem "aarch64-darwin" [ ./hosts/Nigels-Virtual-Machine ];
                 uoK79KQLK7M0 = darwinSystem "aarch64-darwin" [ ./hosts/uoK79KQLK7M0 ];
-#            Nigels-Virtual-Machine = darwin.lib.darwinSystem {
-#                system = "aarch64-darwin";
-#                pkgs = import nixpkgs { system = "aarch64-darwin"; };
-#                specialArgs = {
-#                    inherit inputs nixpkgs-stable nixpkgs-unstable username;
-#                };
-#                modules = [
-#                    nix-homebrew.darwinModules.nix-homebrew {
-#                        nix-homebrew = {
-#                            # Install Homebrew under the default prefix
-#                            enable = true;
-#
-#                            # apple silicon only: also install homebrew under the default intel prefix for rosetta 2
-#                            enablerosetta = false;
-#
-#                            # user owning the homebrew prefix
-#                            user = "${username}";
-#
-#                            # Declarative tap management
-#                            taps = with inputs; {
-#                                "homebrew/homebrew-core" = homebrew-core;
-#                                "homebrew/homebrew-cask" = homebrew-cask;
-#                                "homebrew/homebrew-cask-fonts" = homebrew-cask-fonts;
-#                                "homebrew/homebrew-cask-versions" = homebrew-cask-versions;
-##                                "homebrew/homebrew-bundle" = homebrew-bundle;
-##                                "homebrew/homebrew-services" = homebrew-services;
-##                                "homebrew/homebrew-cask-drivers" = homebrew-cask-drivers;
-#                            };
-#
-#                            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-#                            mutableTaps = false;
-#
-#                            # automatically migrate existing homebrew installations
-#                            automigrate = false;
-#                        };
-#
-#                    }
-#                    ./modules/darwin
-#                    home-manager.darwinModules.home-manager {
-#                        home-manager = {
-#                            useGlobalPkgs = true;
-#                            useUserPackages = true;
-#                            users."${username}".imports = [
-#                                ./modules/home-manager
-#                            ];
-#                            extraSpecialArgs = {
-#                                inherit username;
-#                            };
-#                        };
-#                    }
-#                ];
-#            };
-#            uoK79KQLK7M0 = let
-#                username = "stani07p";
-#            in darwin.lib.darwinSystem {
-#                system = "aarch64-darwin";
-#                pkgs = import nixpkgs { system = "aarch64-darwin"; };
-#                specialArgs = {
-#                    inherit inputs nixpkgs-stable nixpkgs-unstable username;
-#                };
-#                modules = [
-#                    ./modules/darwin
-#                    home-manager.darwinModules.home-manager {
-#                        home-manager = {
-#                            useGlobalPkgs = true;
-#                            useUserPackages = true;
-#                            users."${username}".imports = [
-#                                ./modules/home-manager
-#                            ];
-#                            extraSpecialArgs = {
-#                                inherit username;
-#                            };
-#                        };
-#                    }
-#                ];
-#            };
             };
         };
 }
