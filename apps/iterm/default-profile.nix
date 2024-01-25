e9d9bbabf79da90d9f76d13c6820186ef1b3aff4
nstanger <nigel.stanger@otago.ac.nz>
2024-01-25 22:12:02 +1300

    Fixed iTerm default profile

--- apps/iterm/default-profile.nix
+++ apps/iterm/default-profile.nix
@@ -1,12 +1,130 @@
-# non default value only
+# Need to include ALL values, even unchanged ones, otherwise iTerm crashes
+# when you open its settings while a terminal window is open. Settings open
+# normally if there are no terminal windows.
 {
     "ASCII Anti Aliased" = 1;
+    "ASCII Ligatures" = 0;
     "AWDS Pane Directory" = "";
     "AWDS Pane Option" = "Recycle";
     "AWDS Tab Directory" = "";
     "AWDS Tab Option" = "Recycle";
     "AWDS Window Directory" = "";
     "AWDS Window Option" = "No";
+    "Ambiguous Double Width" = 0;
+    "Ansi 0 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0;
+        "Color Space" = "sRGB";
+        "Green Component" = 0;
+        "Red Component" = 0;
+    };
+    "Ansi 1 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0;
+        "Color Space" = "sRGB";
+        "Green Component" = 0;
+        "Red Component" = 0.6;
+    };
+    "Ansi 10 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.8509803922;
+        "Red Component" = 0;
+    };
+    "Ansi 11 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.9019607843;
+        "Red Component" = 0.9019607843;
+    };
+    "Ansi 12 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 1;
+        "Color Space" = "sRGB";
+        "Green Component" = 0;
+        "Red Component" = 0;
+    };
+    "Ansi 13 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0.9019607843;
+        "Color Space" = "sRGB";
+        "Green Component" = 0;
+        "Red Component" = 0.9019607843;
+    };
+    "Ansi 14 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0.9019607843;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.9019607843;
+        "Red Component" = 0;
+    };
+    "Ansi 15 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0.9019607843;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.9019607843;
+        "Red Component" = 0.9019607843;
+    };
+    "Ansi 2 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.6509803922;
+        "Red Component" = 0;
+    };
+    "Ansi 3 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.6;
+        "Red Component" = 0.6;
+    };
+    "Ansi 4 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0.6980392157;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.1457623839378357;
+        "Red Component" = 0;
+    };
+    "Ansi 5 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0.6980392157;
+        "Color Space" = "sRGB";
+        "Green Component" = 0;
+        "Red Component" = 0.6980392157;
+    };
+    "Ansi 6 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0.6980392157;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.6509803922;
+        "Red Component" = 0;
+    };
+    "Ansi 7 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0.7490196078;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.7490196078;
+        "Red Component" = 0.7490196078;
+    };
+    "Ansi 8 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0.4;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.4;
+        "Red Component" = 0.4;
+    };
+    "Ansi 9 Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0;
+        "Color Space" = "sRGB";
+        "Green Component" = 0;
+        "Red Component" = 0.9019607843;
+    };
+    "Application Keypad Allowed" = 0;
+    "BM Growl" = 0;
     "Background Color" = {
         "Alpha Component" = 1;
         "Blue Component" = 1;
@@ -14,10 +132,19 @@
         "Green Component" = 1;
         "Red Component" = 1;
     };
+    "Background Image Location" = "";
+    "Badge Color" = {
+        "Alpha Component" = 0.5;
+        "Blue Component" = 0;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.14910027384758;
+        "Red Component" = 1;
+    };
+    "Badge Text" = "";
+    "Blink Allowed" = 0;
     "Blinking Cursor" = 1;
     Blur = 1;
     "Blur Radius" = 25;
-    "BM Growl" = 0;
     "Bold Color" = {
         "Alpha Component" = 1;
         "Blue Component" = 1;
@@ -26,13 +153,23 @@
         "Red Component" = 0;
     };
     "Brighten Bold Text" = 0;
+    "Character Encoding" = 4;
+    "Close Sessions On End" = 1;
     Columns = 99;
+    Command = "/bin/bash --login";
     "Cursor Color" = {
         "Alpha Component" = 1;
         "Blue Component" = 0;
         "Color Space" = "sRGB";
         "Green Component" = 0;
         "Red Component" = 0;
+    };
+    "Cursor Guide Color" = {
+        "Alpha Component" = 0.2470588235294118;
+        "Blue Component" = 1;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.9268137812614441;
+        "Red Component" = 0.7021402716636658;
     };
     "Cursor Text Color" = {
         "Alpha Component" = 1;
@@ -41,7 +178,14 @@
         "Green Component" = 0.9999999403953552;
         "Red Component" = 1;
     };
+    "Custom Command" = "No";
     "Custom Directory" = "Advanced";
+    "Default Bookmark" = "No";
+    Description = "Default";
+    "Disable Window Resizing" = 1;
+    "Draw Powerline Glyphs" = 0;
+    "Enable Triggers in Interactive Apps" = 0;
+    "Flashing Bell" = 1;
     "Foreground Color" = {
         "Alpha Component" = 1;
         "Blue Component" = 0;
@@ -49,10 +193,17 @@
         "Green Component" = 0;
         "Red Component" = 0;
     };
-    Description = "Default";
-    "Enable Triggers in Interactive Apps" = 0;
-    "Flashing Bell" = 1;
+    "Hide After Opening" = 0;
+    "Horizontal Spacing" = 1;
+    "Idle Code" = 0;
+    "Initial Text" = "";
     "Initial Use Transparency" = 1;
+    "Jobs to Ignore" = [
+        "rlogin"
+        "ssh"
+        "slogin"
+        "telnet"
+    ];
     # among other things, delete ^2 through ^8 as those are used to
     # switch workspaces
     "Keyboard Map" = {
@@ -174,11 +325,29 @@
         };
     };
     "Left Option Key Changeable" = 0;
+    "Link Color" = {
+        "Alpha Component" = 1;
+        "Blue Component" = 0.7342271208763123;
+        "Color Space" = "sRGB";
+        "Green Component" = 0.3591529726982117;
+        "Red Component" = 0;
+    };
+    "Minimum Contrast" = 0;
+    "Mouse Reporting" = 1;
     "Movement Keys Scroll Outside Interactive Apps" = 1;
     Name = "Default";
+    "Non Ascii Font" = "Monaco 12";
+    "Non-ASCII Anti Aliased" = 1;
     "Normal Font" = "HackNFM-Regular 13";
+    "Only The Default BG Color Uses Transparency" = 0;
+    "Open Toolbelt" = 0;
+    "Option Key Sends" = 0;
+    "Place Prompt at First Column" = 1;
+    "Prompt Before Closing 2" = 0;
+    "Right Option Key Sends" = 0;
     Rows = 40;
     "Scrollback Lines" = 2000;
+    Screen = "-1";
     "Selected Text Color" = {
         "Alpha Component" = 1;
         "Blue Component" = 0;
@@ -193,9 +362,20 @@
         "Green Component" = 0.5019607843;
         "Red Component" = 1;
     };
+    "Send Code When Idle" = 0;
+    Shortcut = "";
+    "Show Mark Indicators" = 1;
     "Show Status Bar" = 1;
     "Show Timestamps" = 2;
+    "Silence Bell" = 0;
+    "Smart Cursor Color" = 0;
     "Status Bar Layout" = {
+        "advanced configuration" = {
+            algorithm = 0;
+            "auto-rainbow style" = 0;
+            font = ".AppleSystemUIFont 12";
+            "remove empty components" = 0;
+        };
         components = [
             {
                 class = "iTermStatusBarJobComponent";
@@ -265,8 +445,11 @@
             }
         ];
     };
-    "Title Components" = 32;
+    "Sync Title" = 0;
+    Tags = [  ];
+    "Terminal Type" = "xterm-256color";
     "Thin Strokes" = 1;
+    "Title Components" = 32;
     Transparency = 0.15;
     Triggers = [
         {
@@ -295,6 +478,17 @@
             regex = "^.+[$#] ?$";
         }
     ];
+    "Unlimited Scrollback" = 0;
+    "Use Bold Font" = 1;
+    "Use Bright Bold" = 1;
+    "Use Cursor Guide" = 0;
+    "Use Custom Window Title" = 0;
+    "Use Italic Font" = 1;
     "Use Non-ASCII Font" = 0;
+    "Use Tab Color" = 0;
+    "Use Underline Color" = 0;
+    "Vertical Spacing" = 1;
+    "Visual Bell" = 1;
+    "Window Type" = 0;
     "Working Directory" = "";
 }
