5f646e5f2940ecfc67f0a4f1a475462322468ca6
stani07p <nigel.stanger@otago.ac.nz>
2024-01-26 13:54:06 +1300

    Added eduVPN config

---
+++ apps/eduvpn.nix
@@ -0,0 +1,6 @@
+{
+    isPrivacyDisclaimerAccepted = 1;
+    isStatusItemInColor = 1;
+    launchAtLogin = 1;
+    showInDock = 0;
+}
--- darwin/mas-apps-common.nix
+++ darwin/mas-apps-common.nix
@@ -1,6 +1,6 @@
 {
     Amphetamine = 937984704;
-    "eduVPN client" = 1317704208; # not configured
+    "eduVPN client" = 1317704208;
     Fantastical = 975937182;
     MsgFiler = 418778021;
     PCalc = 403504866;
