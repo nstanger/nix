30be0ef7248bca86af5483632be77da46fd86d12
stani07p <nigel.stanger@otago.ac.nz>
2024-02-01 15:26:26 +1300

    Updated spotlight settings

--- apps/spotlight.nix
+++ apps/spotlight.nix
@@ -7,7 +7,15 @@
         }
         {
             enabled = 0;
-            name = "MENU_SPOTLIGHT_SUGGESTIONS";
+            name = "BOOKMARKS";
+        }
+        {
+            enabled = 0;
+            name = "MENU_EXPRESSION";
+        }
+        {
+            enabled = 0;
+            name = "CONTACT";
         }
         {
             enabled = 0;
@@ -15,63 +23,23 @@
         }
         {
             enabled = 0;
-            name = "MENU_EXPRESSION";
-        }
-        {
-            enabled = 1;
             name = "MENU_DEFINITION";
         }
         {
             enabled = 0;
-            name = "SYSTEM_PREFS";
+            name = "SOURCE";
         }
         {
             enabled = 1;
             name = "DOCUMENTS";
         }
         {
-            enabled = 1;
-            name = "DIRECTORIES";
-        }
-        {
-            enabled = 1;
-            name = "PRESENTATIONS";
-        }
-        {
             enabled = 0;
-            name = "SPREADSHEETS";
-        }
-        {
-            enabled = 1;
-            name = "PDF";
-        }
-        {
-            enabled = 1;
-            name = "MESSAGES";
-        }
-        {
-            enabled = 1;
-            name = "CONTACT";
-        }
-        {
-            enabled = 1;
             name = "EVENT_TODO";
         }
         {
             enabled = 1;
-            name = "IMAGES";
-        }
-        {
-            enabled = 0;
-            name = "BOOKMARKS";
-        }
-        {
-            enabled = 0;
-            name = "MUSIC";
-        }
-        {
-            enabled = 0;
-            name = "MOVIES";
+            name = "DIRECTORIES";
         }
         {
             enabled = 0;
@@ -79,11 +47,47 @@
         }
         {
             enabled = 1;
+            name = "IMAGES";
+        }
+        {
+            enabled = 1;
+            name = "MESSAGES";
+        }
+        {
+            enabled = 0;
+            name = "MOVIES";
+        }
+        {
+            enabled = 0;
+            name = "MUSIC";
+        }
+        {
+            enabled = 1;
             name = "MENU_OTHER";
         }
         {
             enabled = 1;
-            name = "SOURCE";
+            name = "PDF";
+        }
+        {
+            enabled = 1;
+            name = "PRESENTATIONS";
+        }
+        {
+            enabled = 0;
+            name = "MENU_SPOTLIGHT_SUGGESTIONS";
+        }
+        {
+            enabled = 1;
+            name = "SPREADSHEETS";
+        }
+        {
+            enabled = 1;
+            name = "SYSTEM_PREFS";
+        }
+        {
+            enabled = 0;
+            name = "TIPS";
         }
     ];
 }
