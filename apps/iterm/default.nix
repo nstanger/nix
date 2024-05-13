52b53c3f04967571f4e09bd85bf94f90524f4d62
Nigel Stanger <nigel.stanger@otago.ac.nz>
2024-05-13 17:10:42 +1200

    Added a couple of settings for tmux

--- apps/iterm/default.nix
+++ apps/iterm/default.nix
@@ -1,4 +1,5 @@
 {
+    AutoHideTmuxClientSession = 1;
     DimBackgroundWindows = 1;
     DisableWindowSizeSnap = 1;
     EnableProxyIcon = 1;
@@ -9,7 +10,11 @@
     NoSyncTurnOffFocusReportingOnHostChange = 1;
     NoSyncTurnOffMouseReportingOnHostChange = 1;
     OpenFileInNewWindows = 1;
+    SeparateWindowTitlePerTab = 0;
     ShowFullScreenTabBar = 1;
+    # Can't remember whether "Show per-pane title bar with split panes" was
+    # enabled but default, but I can't find it in defaults anyway :/.
+    # It should be enabled.
     StatusBarPosition = 1;
     SUEnableAutomaticChecks = 1;
     SUSendProfileInfo = 0;
