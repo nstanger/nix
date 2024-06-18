16e6e334832acbd437b4dcb96a5aec6b8aaba762
Nigel Stanger <nigel.stanger@otago.ac.nz>
2024-06-18 14:21:21 +1200

    Added LibreOffice mappings

--- darwin/utis.nix
+++ darwin/utis.nix
@@ -24,4 +24,28 @@
         "public.xml"
         "public.yaml"
     ];
+    "org.libreoffice.script" = [
+        "org.oasis-open.opendocument.chart"
+        "org.oasis-open.opendocument.chart-template"
+        "org.oasis-open.opendocument.database"
+        "org.oasis-open.opendocument.formula"
+        "org.oasis-open.opendocument.formula-template"
+        "org.oasis-open.opendocument.graphics"
+        "org.oasis-open.opendocument.graphics-template"
+        "org.oasis-open.opendocument.image"
+        "org.oasis-open.opendocument.image-template"
+        "org.oasis-open.opendocument.presentation"
+        "org.oasis-open.opendocument.presentation-template"
+        "org.oasis-open.opendocument.spreadsheet"
+        "org.oasis-open.opendocument.spreadsheet-template"
+        "org.oasis-open.opendocument.text"
+        "org.oasis-open.opendocument.text-master"
+        "org.oasis-open.opendocument.text-master-template"
+        "org.oasis-open.opendocument.text-template"
+        "org.oasis-open.opendocument.text-web"
+        "vnd.oasis.opendocument.graphics-flat-xml"
+        "vnd.oasis.opendocument.presentation-flat-xml"
+        "vnd.oasis.opendocument.spreadsheet-flat-xml"
+        "vnd.oasis.opendocument.text-flat-xml"
+    ];
 }
