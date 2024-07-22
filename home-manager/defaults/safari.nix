43b8c13f02fa02bc2e0963fd569db6ff0e6f89e7
Nigel Stanger <nigel.stanger@otago.ac.nz>
2024-07-22 15:31:38 +1200

    Added more Safari defaults

--- home-manager/defaults/safari.nix
+++ home-manager/defaults/safari.nix
@@ -1,14 +1,17 @@
 {
+    AllowJavaScriptFromAppleEvents = true;
     AutoFillCreditCardData = false;
     AutoFillFromAddressBook = false;
     AutoFillMiscellaneousForms = false;
     AutoFillPasswords = false;
     # Prevent Safari from opening ‘safe’ files automatically after downloading
     AutoOpenSafeDownloads = false;
+    HomePage = "";
     IncludeDevelopMenu = true;
     IncludeInternalDebugMenu = true;
     SuppressSearchSuggestions = true;
     # ShowFavoritesBar = false;
+    ShowFavoritesUnderSmartSearchField = false;
     ShowFullURLInSmartSearchField = true;
     ShowOverlayStatusBar = true;
     # Privacy: don’t send search queries to Apple
@@ -16,8 +19,11 @@
     WarnAboutFraudulentWebsites = true;
     WebAutomaticSpellingCorrectionEnabled = false;
     WebContinuousSpellCheckingEnabled = true;
+    WebKitDefaultTextEncodingName = "utf-8";
     WebKitJavaEnabled = false;
     WebKitJavaScriptCanOpenWindowsAutomatically = false;
+    "WebKitPreferences.applePayCapabilityDisclosureAllowed" = false;
+    "WebKitPreferences.defaultTextEncodingName" = "utf-8";
     # Press Tab to highlight each item on a web page
     # WebKitTabToLinksPreferenceKey = true;
     # "com.apple.Safari.ContentPageGroupIdentifier.WebKit2TabsToLinks" = true;
