bea3ffb7ba8317998ded017dbe27a23e5505ba2a
Nigel Stanger <nigel.stanger@otago.ac.nz>
2024-11-15 15:44:04 +1300

    Added node-slim to shut up Sonarlint VS Code extension

--- home-manager/packages-common.nix
+++ home-manager/packages-common.nix
@@ -11,6 +11,8 @@ pkgs: with pkgs; [
     imagemagick
     mkcert
     # neovide
+    # Need node to shut up the VS Code Sonarlint extension.
+    nodejs-slim_22
     nodePackages.tiddlywiki
     p7zip
     plantuml
