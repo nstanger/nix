d1cfa7ad27d521b3e2607ef204e69a98f67c6201
Nigel Stanger <nigel.stanger@otago.ac.nz>
2025-01-08 21:31:45 +1300

    Corrected sorting

--- homebrew/homebrew-casks-common.nix
+++ homebrew/homebrew-casks-common.nix
@@ -17,9 +17,9 @@
     "inkscape" # install fails under home-manager, config not in defaults
     "insomnia" # no config
     "keepassxc" # no config
-    "localsend"
     "letter-opener"
     "libreoffice" # config not in defaults
+    "localsend"
     "mactex-no-gui" # this appears to be ... complicated with nixpkgs
     "netbeans" # config not in defaults
     "pacifist"
