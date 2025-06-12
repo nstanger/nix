f5985b72e6c96649e3fccc9778defd35040562ce
Nigel Stanger <nigel.stanger@otago.ac.nz>
2025-06-12 15:10:07 +1200

    Added note about Office defaults

--- home-manager/defaults/office.nix
+++ home-manager/defaults/office.nix
@@ -7,5 +7,7 @@
     OptionalConnectedExperiencesPreference = 0;
     SendAllTelemetryEnabled = 0;
     ShowDocStageOnLaunch = 0;
+    # apparently the trick is to enable this via defaults, then toggle
+    # it off and on again in the GUI, oy
     ShowFileExtensions = 1;
 }
