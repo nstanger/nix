aaff7fd1a8e9bc2d12c824b8bea8649da33617ec
Nigel Stanger <nigel.stanger@otago.ac.nz>
2025-08-27 13:27:36 +1200

    Configured “all spaces” apps

---
+++ home-manager/defaults/spaces.nix
@@ -0,0 +1,18 @@
+{
+    "app-bindings" = {
+        "com.apple.activitymonitor" = "AllSpaces";
+        "com.apple.printcenter" = "AllSpaces";
+        "com.binarynights.forklift" = "AllSpaces";
+        "com.crystalidea.macsfancontrol" = "AllSpaces";
+        "com.electron.dockerdesktop" = "AllSpaces";
+        "com.getdropbox.dropbox" = "AllSpaces";
+        "com.google.drivefs" = "AllSpaces";
+        "com.if.amphetamine" = "AllSpaces";
+        "com.logitech.logitune" = "AllSpaces";
+        "com.microsoft.onedrive" = "AllSpaces";
+        "com.synology.CloudStation" = "AllSpaces";
+        "net.sourceforge.skim-app.skim" = "AllSpaces";
+        "org.eduvpn.app" = "AllSpaces";
+        "uk.co.tla-systems.pcalc" = "AllSpaces";
+    };
+}
